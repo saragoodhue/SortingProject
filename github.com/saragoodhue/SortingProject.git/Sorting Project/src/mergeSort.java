3e65b726ed1fd8e52a5e482d0c54177a53b615dc
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2021-11-30 14:55:34 -0700

    first attempt with random number gen

---
+++ github.com/saragoodhue/SortingProject.git/Sorting Project/src/mergeSort.java
@@ -0,0 +1,11 @@
+
+public class mergeSort
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+	}
---
+++ github.com/saragoodhue/SortingProject.git/Sorting Project/src/sortingRun.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class sortingRun
+	{
+
+		public static void main(String[] args)
+			{
+				insertionSort.insertionSort(null);
+				mergeSort.merge(null, 0, 0, 0, null);
+				quickSort.qsort(null);
+				selectionSort.selectionSort(null);
+				
+				userChoice();
+			}
+		
+		public static void userChoice()
+			{
+				Scanner numberOfRuns = new Scanner(System.in);
+				System.out.println("How many times would you like the simulation to run?");
+				int runs = numberOfRuns.nextInt();
+				
+				Scanner numberOfIntegers = new Scanner(System.in);
+				System.out.println("How many times would you like the simulation to run?");
+				int nums = numberOfIntegers.nextInt();
+				
+				
+				for(int i = 0; i < nums; i++)
+					{
+						int randomNums = (int) (Math.random()*88) + 10;
+						int line = randomNums;
+				        int [] newArray = line.split(" "); // taking numbers and putting them into the array
+				        
+				        ArrayList<String> uniqueNums = new ArrayList<String>();
+					}
+			}
+
+	}
